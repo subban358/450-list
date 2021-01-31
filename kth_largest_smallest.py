0829775c1b1c7300ea8799ae7145b24060353888
subban358 <subhambanerjee138@gmail.com>
2021-01-31 22:36:59 +0530

    Quick Select

--- kth_largest_smallest.py
+++ kth_largest_smallest.py
@@ -1,3 +1,5 @@
+###################### Heap Method ###################
+
 import heapq as hq
 class Solution:
     def kthLargest(self, arr, k):
@@ -22,5 +24,40 @@ class Solution:
     
     
         return hq.heappop(l)
-            
+
+
+################ Quick Select Method ################################
+
+
+def partition(arr, lo, hi):
+    i, j = lo - 1, lo
+    pivot = arr[hi]
+    for j in range(lo, hi):
+        if arr[j] < pivot:
+            i += 1
+            arr[j], arr[i] = arr[i], arr[j]
+    i += 1
+    arr[i], arr[hi] = arr[hi], arr[i]
+    
+    return i
+    
+def quickSelect(arr, l, r, k):
+    
+    pivotIndex = partition(arr, l, r)
+    if pivotIndex  < k:
+        return quickSelect(arr, pivotIndex + 1, r, k)
+    elif pivotIndex > k:
+        return quickSelect(arr, l, pivotIndex - 1, k)
+    else:
+        return arr[pivotIndex]
+    
+def kthSmallest(arr, l, r, k):
+    '''
+    arr : given array
+    l : starting index of the array i.e 0
+    r : ending index of the array i.e size-1
+    k : find kth smallest element and return using this function
+    '''
+    return quickSelect(arr, l, r, k-1)
+
                 
